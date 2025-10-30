cd2e441b787b0fd11e19d277ed1f127371bb89c5
Oscar Lara <oscar.lara.a@gmail.com>
2025-10-30 18:31:56 -0500

    se agregaron las vistas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,9 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_recipe, only: [:show, :destroy]
+
   def index
-    @recipes = Recipe.joins(:chat).where(chats: { user_id: current_user.id })
+    @recipes = current_user.recipes
   end
 
   def show
@@ -13,7 +14,9 @@ class RecipesController < ApplicationController
     redirect_to recipes_path, notice: "Receta eliminada correctamente."
   end
 
+  private
+
   def set_recipe
-    @recipe = Recipe.find(params[:id])
+    @recipe = current_user.recipes.find(params[:id])
   end
 end
