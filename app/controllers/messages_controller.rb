632836e62f8f4872027f57a0ef305ab74cc5cc52
gonzalo aime <aimegonzalo@gmail.com>
2025-10-30 11:53:58 +0000

    cambios en los archivos routes, controlador y clase messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,34 @@
 class MessagesController < ApplicationController
   before_action :set_chat
+
   def create
     @message = @chat.messages.build(message_params) #.build es el equivalente a hacer @message = Message.new(message_params) + @message.chat = @chat
+
     if @message.save
       respond_to do |format|
-        format.html {redirect_to chat_path(@chat)}
-        format.turbo_srtream
+        format.html { redirect_to chat_path(@chat) }
+        format.turbo_stream
       end
     else
       respond_to do |format|
-        format.html {render "chats/show", status: :unprocessable_entity}
-        format.turbo_srtream {render turbo_stream: turbo_stream.replace"message_form", partial: "messages/form", locals: { chat: @chat, message: @message }) }
+        format.html { render "chats/show", status: :unprocessable_entity }
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace(
+            "message_form",
+            partial: "messages/form",
+            locals: { chat: @chat, message: @message }
+          )
+        }
       end
     end
   end
 
   private
-  def set_chat"message_form", partial: "messages/form", locals: { chat: @chat, message: @message })
+
+  def set_chat
     @chat = Chat.find(params[:chat_id])
   end
+
   def message_params
     params.require(:message).permit(:content)
   end
