89e2662db71a6cce37ab057f856afafefbe4de8a
Daniel Ferrero <danielferrerobaa@gmail.com>
2025-10-31 00:01:13 +0000

    Correccion para vistas chats

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,8 @@ class MessagesController < ApplicationController
 
   def create
     @message = @chat.messages.build(message_params)
-
+    @message.user = current_user
+    
     if @message.save
       redirect_to chat_path(@chat), notice: "Mensaje creado correctamente."
     else
