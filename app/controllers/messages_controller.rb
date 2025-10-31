9154b1a45f1c7b74c291ae2df7476e57ce986520
Daniel Ferrero <danielferrerobaa@gmail.com>
2025-10-31 00:08:34 +0000

    Revert "Correccion para vistas chats"
    This reverts commit 89e2662db71a6cce37ab057f856afafefbe4de8a.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @chat.messages.build(message_params)
-    @message.user = current_user
-    
+
     if @message.save
       redirect_to chat_path(@chat), notice: "Mensaje creado correctamente."
     else
