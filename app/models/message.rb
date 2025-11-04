2ea85791394f27751fda80da21511eaeba3997a1
gonzalo aime <aimegonzalo@gmail.com>
2025-11-04 23:34:24 +0000

    audios y validaciones completo

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,17 @@ class Message < ApplicationRecord
   belongs_to :user, optional: true  #sin el optional true, no me estaba respondiendo la IA, porque los mensajes de la IA no tienen un USER
   has_one_attached :file
   validates :content, presence: true
+validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
+  validate :file_size_validation
+
+  MAX_FILE_SIZE_MB = 10
+
+  private
+
+  def file_size_validation
+    if file.attached? && file.byte_size > MAX_FILE_SIZE_MB.megabytes
+      errors.add(:file, "size must be less than #{MAX_FILE_SIZE_MB}MB")
+    end
+  end
+
 end
